09966939054887f53ee7676663a1af37b8b28c69
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2025-10-06 22:34:49 -0700

    feat: add getComputerChoice function
    Implements a function called getComputerChoice so that the computer
    randomly chooses one of the following: "rock", "paper", or "scissors"
    and returns it to the console.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Create a function
+function getComputerChoice() {
 // Create an array of possible choices
+  const choices = ["rock", "paper", "scissors"];
 // Generate a random index
+  const randomIndex = Math.floor(Math.random() * choices.length);
 // Return one random choice
+  return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
